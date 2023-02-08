481647993e0d501fd31821aef69194d8f1542794
Jay Gillenwater <55106933+jhgille2@users.noreply.github.com>
2023-02-08 16:00:50 -0500

    Update e5.sh

--- e5.sh
+++ e5.sh
@@ -15,6 +15,9 @@ tar -xzf YeastAssembly.tar.gz
 # Concatenate the paired reads into one single-end file
 cat ./YeastAssembly/yeastCh15.read1.fq ./YeastAssembly/yeastCh15.read2.fq > yeastCh15.readsAll.fq
 
+# fastqc
+fastqc ./YeastAssembly/yeastCh15.read1.fq ./YeastAssembly/yeastCh15.read2.fq
+
 # Make a directory to do the assembly in 
 mkdir ./masurca_assembly
 cd ./masurca_assembly
@@ -29,12 +32,19 @@ wget https://raw.githubusercontent.com/jhgille2/cs590_2023/main/exercize_5_masur
 # Start the assembly
 nohup ./assemble.sh &
 
+BACK_PID=$!
+while kill -0 $BACK_PID ; do
+    echo "Asssembler be assemblin..."
+    sleep 30
+    # You can add a timeout here if you want
+done
+
 # Visualize assembly against reference genome with Mummer
 # 0. Install gnuplot
 sudo apt install gnuplot
 
 # 1. Make out.delta file
-nucmer /home/jhgille2/YeastAssembly/S288C_reference_Chr15.fa ./CA/final.genome.scf.fasta
+nucmer $HOME/YeastAssembly/S288C_reference_Chr15.fa ./CA/final.genome.scf.fasta
 
 # 2. Run mummerplot to make the plot from the out.delta file
 mummerplot -l -p MyAlignment --png --large out.delta
@@ -44,7 +54,7 @@ mummerplot -l -p MyAlignment --png --large out.delta
 # PART 2: re-run with paired end reads
 
 # Go back to the starting directory
-cd /home/jhgille2
+cd $HOME
 
 # make a new directory for the second alignment
 mkdir masurca_assembly_paired
@@ -60,10 +70,16 @@ wget https://raw.githubusercontent.com/jhgille2/cs590_2023/main/exercize_5_masur
 
 # Run the assembly
 nohup ./assemble.sh &
+BACK_PID=$!
+while kill -0 $BACK_PID ; do
+    echo "Asssembler be assemblin..."
+    sleep 30
+    # You can add a timeout here if you want
+done
 
 # Visualize assembly against reference genome with Mummer
 # 1. Make out.delta file
-nucmer /home/jhgille2/YeastAssembly/S288C_reference_Chr15.fa ./CA/final.genome.scf.fasta
+nucmer $HOME/YeastAssembly/S288C_reference_Chr15.fa ./CA/final.genome.scf.fasta
 
 # 2. Run mummerplot to make the plot from the out.delta file
 mummerplot -l -p MyAlignment --png --large out.delta
