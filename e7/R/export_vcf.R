dd7db42de91c7b367e3e2539e5f3fa34e7e2d971
Jay Gillenwater <jhgille2@ncsu.edu>
2023-02-22 16:23:27 -0500

    updated to output a gzipped filtered vcf

--- e7/R/export_vcf.R
+++ e7/R/export_vcf.R
@@ -6,7 +6,7 @@
 #' @param nondist_vcf
 export_vcf <- function(nondist_vcf) {
 
-  outpath <- here("exports", "out_filtered.vcf")
+  outpath <- here("exports", "out_filtered.vcf.gz")
   
   write.vcf(nondist_vcf, 
             file = outpath)
