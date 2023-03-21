ab77e55e177fe18baf67b101f057cb482546e487
Jay Gillenwater <jhgille2@ncsu.edu>
2023-03-21 09:08:29 -0400

    add e11

---
+++ e11/packages.R
@@ -0,0 +1,15 @@
+# Workflow management
+pacman::p_load(targets,
+               tarchetypes,
+               here)
+
+# Data wrangling
+pacman::p_load(dplyr,
+               purrr,
+               magrittr,
+               tools,
+               tibble,
+               htmltools)
+
+
+
